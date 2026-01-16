25f345c8e6b216325cd5438a3c9b65867a899aa5
Arnav 50 <arnavthakur13456@gmail.com>
2026-01-16 15:52:08 +0530

    changes done im Loactorfiles

--- AtuomationTestCases/Locator.Spec.js
+++ AtuomationTestCases/Locator.Spec.js
@@ -1,32 +1,30 @@
-// page.getByAltText() - to locate an element, usually image, by its text alternative.
-// page.getByPlaceholder() - to locate an input by placeholder.
-// page.getByRole() to locate by explicit and implicit accessibility attributes.
-// page.getByText() to locate by text content.
+const{test, epect} = require('@playwright/test');
 
-// page.getByLabel() to locate a form control by associated label's text.
-// page.getByTitle() to locate an element by its title attribute.
-// page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+test('Locate Multiple elements', async ({page}) => {
 
-const {test, expect} = require('@playwright/test');
+    await page.goto('https://demoblaze.com/')
 
-test('All Locator Methods', async({page}) =>{
+//<-----------------Check all Links in the page--------------------------->
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+const allLinks = await page.$$('a');
 
-    // page.getByAltText() - to locate an element, usually image, by its text alternative.
-    const Loogo = await page.getByAltText('company-branding')
-    await expect(Loogo).toBeVisible();
+for(const Link of allLinks){
+    const allLinkText = await Link.textContent();
+    console.log('Link Verify :', allLinkText);
+}
 
-    // page.getByPlaceholder() - to locate an input by placeholder.
-    await page.getByPlaceholder('Username').fill('Admin');
-    await page.getByPlaceholder('Password').fill('admin123');
+//<-----------------Check all Products in the page--------------------------->
 
-    // page.getByRole() to locate by explicit and implicit accessibility attributes. like button, link, checkbox, textbox etc.
-    await page.getByRole('button', {type: 'submit'}).click();
-    
-    // page.getByText() to locate by text content.
-    const dashboard = await page.locator("//p[@class='oxd-userdropdown-name']");
-    console.log('Login User is :', await dashboard.textContent());
-   
+await page.waitForSelector("//div[@id='tbodyid']//div//h4/a");
+
+const allProducts = await page.$$("//div[@id='tbodyid']//div//h4/a");
+
+for(const product of allProducts){
+    const allProductName = await product.textContent();
+    console.log('Product Verify :', allProductName);
+}
+
+page.close();
 
 })
+
